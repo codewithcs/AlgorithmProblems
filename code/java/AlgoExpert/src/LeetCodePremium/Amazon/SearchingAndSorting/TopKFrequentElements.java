a08cd80af033290e9e6d8fe8c4ba32764070fb23
codewithcs <shikhar1chaudhary@gmail.com>
2020-10-30 10:11:18 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Amazon/SearchingAndSorting/TopKFrequentElements.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Amazon/SearchingAndSorting/TopKFrequentElements.java
@@ -52,7 +52,7 @@ public class TopKFrequentElements {
         return list.stream().mapToInt(i -> i).toArray();
     }
 
-    // Approach 2:
+    // Approach 2: Important Case: HashMap having duplicate values. 1 --> 1, 2 --> 1
     public static int[] topKFrequent2(int[] nums, int k) {
         TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
         for (int num : nums) {
@@ -70,7 +70,7 @@ public class TopKFrequentElements {
                 if(e1.getValue() == e2.getValue()){
                     return -1;
                 } else {
-                    return e2.getValue().compareTo(e1.getValue());
+                    return e2.getValue().compareTo(e1.getValue()); // need in descending order.
                 }
             }
         });
@@ -85,5 +85,154 @@ public class TopKFrequentElements {
         }
 
         return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    /// Approach 3: Using a Heap. 1 ≤ k ≤ number of unique elements.
+    public int[] topKFrequent3(int[] nums, int k) {
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
+        }
+
+        // 1. build hash map : character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first'
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k){
+                heap.poll();
+            }
+        }
+        // In 2nd step, to build a heap of size k using n elements.
+
+        // 3. build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
+    }
+
+    // Approach 4: Using Quick Select : Fastest
+    int[] unique; // Over writing the array.
+    Map<Integer, Integer> count;
+
+    public void swap(int a, int b) {
+        int tmp = unique[a];
+        unique[a] = unique[b];
+        unique[b] = tmp;
+    }
+
+    public int partition(int left, int right, int pivot_index) {
+        int pivot_frequency = count.get(unique[pivot_index]);
+        // 1. move pivot to end
+        swap(pivot_index, right);
+        int store_index = left;
+
+        // 2. move all less frequent elements to the left
+        for (int i = left; i <= right; i++) {
+            if (count.get(unique[i]) < pivot_frequency) {
+                swap(store_index, i);
+                store_index++;
+            }
+        }
+
+        // 3. move pivot to its final place
+        swap(store_index, right);
+
+        return store_index;
+    }
+
+    public void quickselect(int left, int right, int k_smallest) {
+        /*
+        Sort a list within left..right till kth less frequent element
+        takes its place.
+        */
+
+        // base case: the list contains only one element
+        if (left == right) return;
+
+        // select a random pivot_index
+        Random random_num = new Random();
+        int pivot_index = left + random_num.nextInt(right - left);
+
+        // find the pivot position in a sorted list
+        pivot_index = partition(left, right, pivot_index);
+
+        // if the pivot is in its final sorted position
+        if (k_smallest == pivot_index) {
+            return;
+        } else if (k_smallest < pivot_index) {
+            // go left
+            quickselect(left, pivot_index - 1, k_smallest);
+        } else {
+            // go right
+            quickselect(pivot_index + 1, right, k_smallest);
+        }
+    }
+
+    public int[] topKFrequent4(int[] nums, int k) {
+        // build hash map : character and how often it appears
+        count = new HashMap();
+        for (int num: nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        // Array of unique elements
+        int n = count.size();
+        unique = new int[n];
+        int i = 0;
+        for (int num: count.keySet()) {
+            unique[i] = num;
+            i++;
+        }
+
+        // kth top frequent element is (n - k)th less frequent.
+        // Do a partial sort: from less frequent to the most frequent, till
+        // (n - k)th less frequent element takes its place (n - k) in a sorted array.
+        // All element on the left are less frequent.
+        // All the elements on the right are more frequent.
+        quickselect(0, n - 1, n - k);
+
+        // Return top k frequent elements
+        return Arrays.copyOfRange(unique, n - k, n);
+    }
+
+    // Approach 5: Using Bucket Sort.
+    public int[] topKFrequent5(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+
+        return res.stream().mapToInt(i->i).toArray();
     }
 }
