057d7bf2118e611de533b5e41f40732104a955cc
codewithcs <shikhar1chaudhary@gmail.com>
2020-11-10 11:24:33 +0000

    Add code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/TreesAndGraph/ConvertBinarySearchTreeToSortedDoublyLinkedList.java
@@ -0,0 +1,22 @@
+package LeetCodePremium.Facebook.TreesAndGraph;
+
+public class ConvertBinarySearchTreeToSortedDoublyLinkedList {
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(){}
+        Node(int val){
+            this.val = val;
+        }
+        Node(int val, Node left, Node right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public Node treeToDoublyList(Node root) {
+        return null;
+    }
+}
