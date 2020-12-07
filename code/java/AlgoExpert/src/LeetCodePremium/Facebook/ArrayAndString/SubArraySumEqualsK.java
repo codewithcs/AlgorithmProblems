40c0fbee3cc63a9605e75d32688d9da1048e4516
codewithcs <shikhar1chaudhary@gmail.com>
2020-12-07 21:34:51 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Facebook/ArrayAndString/SubArraySumEqualsK.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/ArrayAndString/SubArraySumEqualsK.java
@@ -80,8 +80,8 @@ public class SubArraySumEqualsK {
     // Using a Map. O(n) time and O(n) space.
     public int subarraySum4(int[] nums, int k) {
         int count = 0, sum = 0;
-        HashMap<Integer, Integer> map = new HashMap < > ();
-        map.put(0, 1);
+        HashMap<Integer, Integer> map = new HashMap<> ();
+        map.put(0, 1); // Important initialization.
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             if (map.containsKey(sum - k)) {
