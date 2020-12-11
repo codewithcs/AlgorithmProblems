95edb5f451c9679bb2b628a84f62fc14b587bfde
codewithcs <shikhar1chaudhary@gmail.com>
2020-12-11 23:35:42 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Facebook/SortingAndSearching/IntersectionOfTwoArrays.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/SortingAndSearching/IntersectionOfTwoArrays.java
@@ -10,6 +10,9 @@ Each element in the result must be unique.
 The result can be in any order.
  */
 public class IntersectionOfTwoArrays {
+    // O(nlogn + mlogn) time and O(1) space.
+    // If the arrays are sorted then O(n+m) time and O(1) space.
+    // Not considering the space used by return array.
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>();
         Arrays.sort(nums1); Arrays.sort(nums2);
@@ -29,6 +32,29 @@ public class IntersectionOfTwoArrays {
         List<Integer> list = new ArrayList<>(set);
         for(int k=0; k<result.length;k++){
             result[k] = list.get(k);
+        }
+        return result;
+    }
+
+    // O(n+m) time and space.
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int num: nums1){
+            set1.add(num);
+        }
+
+        for(int num: nums2){
+            if(set1.contains(num)){
+                set2.add(num);
+            }
+        }
+
+        int[] result = new int[set2.size()];
+        int i=0;
+        for(int num: set2){
+            result[i] = num; i++;
         }
         return result;
     }
