31eb89a3085e9a7c314a9de9968a5edd3e3bc4f0
codewithcs <shikhar1chaudhary@gmail.com>
2020-12-17 06:22:27 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Facebook/SortingAndSearching/DivideTwoIntegers.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/SortingAndSearching/DivideTwoIntegers.java
@@ -17,7 +17,74 @@ Constraints:
 divisor != 0
  */
 public class DivideTwoIntegers {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE/-1);
+    }
+
+    // Time Limit Exceeded. 
     public int divide(int dividend, int divisor) {
-        return 0; 
+        // Special case: overflow.
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        /* We need to convert both numbers to negatives
+         * for the reasons explained above.
+         * Also, we count the number of negatives signs. */
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+
+        /* Count how many times the divisor has to be added
+         * to get the dividend. This is the quotient. */
+        int quotient = 0;
+        while (dividend - divisor <= 0) {
+            quotient--;
+            dividend -= divisor;
+        }
+
+        /* If there was originally one negative sign, then
+         * the quotient remains negative. Otherwise, switch
+         * it to positive. */
+        if (negatives != 1) {
+            quotient = -quotient;
+        }
+        return quotient;
+    }
+
+    public int divide2(int A, int B) {
+        if (A == 1 << 31 && B == -1) {
+            return (1 << 31) - 1;
+        }
+        int a = Math.abs(A), b = Math.abs(B), res = 0, x = 0;
+        while (a - b >= 0) {
+            for (x = 0; a - (b << x << 1) >= 0; x++);
+            res += 1 << x;
+            a -= b << x;
+        }
+        return (A > 0) == (B > 0) ? res : -res;
+    }
+
+    // Best solution.
+    public int divide3(int A, int B) {
+        if (A == 1 << 31 && B == -1) {
+            return (1 << 31) - 1;
+        }
+        int a = Math.abs(A), b = Math.abs(B), res = 0;
+        for (int x = 31; x >= 0; x--) {
+            if ((a >>> x) - b >= 0) {
+                res += 1 << x;
+                a -= b << x;
+            }
+        }
+        return (A > 0) == (B > 0) ? res : -res;
     }
 }
