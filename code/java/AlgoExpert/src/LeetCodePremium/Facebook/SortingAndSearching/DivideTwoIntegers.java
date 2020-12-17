1f58cc609ba16c3bcf56d827a086e1e3d043d617
codewithcs <shikhar1chaudhary@gmail.com>
2020-12-17 03:55:30 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Facebook/SortingAndSearching/DivideTwoIntegers.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/SortingAndSearching/DivideTwoIntegers.java
@@ -1,4 +1,23 @@
 package LeetCodePremium.Facebook.SortingAndSearching;
+/*
+Given two integers dividend and divisor, divide two integers without
+using multiplication, division, and mod operator.
 
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part.
+For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Note:
+Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+For this problem, assume that your function returns 231 − 1 when the division result overflows.
+
+Constraints:
+-2^31 <= dividend, divisor <= 2^31 - 1
+divisor != 0
+ */
 public class DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        return 0; 
+    }
 }
