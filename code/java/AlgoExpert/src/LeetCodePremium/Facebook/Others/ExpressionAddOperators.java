27499e12ddaa5dae13e54d7efe4ff0d5ad0c7bcc
codewithcs <shikhar1chaudhary@gmail.com>
2020-12-26 21:17:50 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Facebook/Others/ExpressionAddOperators.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/Others/ExpressionAddOperators.java
@@ -1,5 +1,7 @@
 package LeetCodePremium.Facebook.Others;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,35 +22,72 @@ Looks like backtracking as well. Can we optimize backtracking using DP ?
  */
 public class ExpressionAddOperators{
     public static void main(String[] args) {
-        System.out.println(addOperators("1234", 1));
+        System.out.println(checkExpression("1*2*3*4*5", 12));
     }
 
     public static List<String> addOperators(String num, int target) {
         List<String> result = new ArrayList<>();
         String current = "";
         String[] operators = { "", "+", "-", "*" };
-        generateString(0, num, current, operators);
+        generateString(0, num, current, operators, result, target);
         return result;
     }
 
-    public static void generateString(int index, String num, String current, String[] operators){
+    public static void generateString(int index, String num, String current, String[] operators, List<String> result, int target){
         if(index == num.length() -1){
             current += (num.charAt(num.length()-1));
             System.out.println("current is : " + current);
+            if(checkExpression(current, target) ){
+                result.add(current);
+            }
             return;
         }
 
-        for(int i=index ; i< num.length()-1; i++){
-            String original = current;
-            for(String operator: operators){
-                current += (num.charAt(i) + operator);
-                generateString(i+1, num, current, operators);
-                current = new String(original);
-            }
+        String original = current;
+        for(String operator: operators){
+            current += (num.charAt(index) + operator);
+            generateString(index+1, num, current, operators, result, target);
+            current = original;
         }
     }
 
-    public boolean checkExpression(String expression){
-        return true;
+    // not completely correct.
+    public static boolean checkExpression(String expression, int target){
+        int currentNum = 0; int previous = 0; int result = 0;
+        char previousOperator = '+';
+
+        for(int i=0; i< expression.length(); i++){
+            char current = expression.charAt(i);
+
+            if(Character.isDigit(current)){
+                int value = 0; int index = i; boolean startsWithAZero = false; int numDigits = 0;
+                while(i < expression.length() && Character.isDigit(expression.charAt(i))){
+                    value = value*10 + (expression.charAt(i) - '0');
+                    numDigits++;
+                    if(value == 0 && i == index){
+                        startsWithAZero = true;
+                    }
+                    i++;
+                }
+                if(numDigits > 1 && startsWithAZero) return false; // Handle the case "00", "05"
+
+                currentNum = value;
+                i--;
+
+                if(previousOperator == '+' || previousOperator == '-'){
+                    result = result + currentNum;
+                    previous = (previousOperator == '+') ? currentNum : -currentNum;
+                } else {
+                    previous = currentNum*previous;
+                }
+
+            } else { // operator
+                    previousOperator = current;
+                }
+        }
+        result += previous;
+
+        System.out.println("result is : " + result);
+        return result == target;
     }
 }
--- code/java/AlgoExpert/src/LeetCodePremium/Facebook/SimilarQuestions/BasicCalculator2.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Facebook/SimilarQuestions/BasicCalculator2.java
@@ -19,8 +19,7 @@ public class BasicCalculator2 {
     public static void main(String[] args) {
         System.out.println(calculate("1*2-3/4+5*6-7*8+9/10"));
     }
-
-    // Doing some stupid but conceptual mistake for the above case ?
+    
     public static int evaluate(int first, int second, char operator){
         if(operator == '+'){
             return second+first;
@@ -75,5 +74,37 @@ public class BasicCalculator2 {
         }
 
         return numbers.peek();
+    }
+
+    // Without Stack: O(n) time and O(1) space.
+    public int calculate2(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+'; // previous operation
+
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
     }
 }
