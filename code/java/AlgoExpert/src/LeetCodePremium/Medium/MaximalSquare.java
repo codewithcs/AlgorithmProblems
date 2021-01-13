855373bdbd1d79db8334305738f20f4d01018b27
codewithcs <shikhar1chaudhary@gmail.com>
2021-01-13 20:09:49 +0000

    Add code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/MaximalSquare.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/MaximalSquare.java
@@ -102,6 +102,7 @@ public class MaximalSquare {
         return maxsqlen * maxsqlen;
     }
 
+    // Using two 1d arrays.
     public static int maximalSquare4(char[][] matrix) {
         int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
         int[] odd = new int[cols+1];
@@ -129,5 +130,25 @@ public class MaximalSquare {
         }
 
         return maxLength * maxLength;
+    }
+
+    // Using one 1d array. Try to understand this.
+    public int maximalSquare5(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[] dp = new int[cols + 1];
+        int maxsqlen = 0, prev = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int temp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                prev = temp;
+            }
+        }
+        return maxsqlen * maxsqlen;
     }
 }
---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/RobotBoundedInCircle.java
@@ -0,0 +1,22 @@
+package LeetCodePremium.Medium;
+
+/*
+On an infinite plane, a robot initially stands at (0, 0) and faces north.
+The robot can receive one of three instructions:
+
+"G": go straight 1 unit;
+"L": turn 90 degrees to the left;
+"R": turn 90 degrees to the right.
+The robot performs the instructions given in order, and repeats them forever.
+
+Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+
+Constraints:
+1 <= instructions.length <= 100, Important clarifying question to ask.
+instructions[i] is 'G', 'L' or, 'R'.
+ */
+public class RobotBoundedInCircle {
+    public boolean isRobotBounded(String instructions) {
+        return false;
+    }
+}
