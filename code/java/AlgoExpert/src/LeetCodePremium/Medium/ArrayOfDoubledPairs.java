ba342492ec19560e59cb700b3343b568dfac3cf5
codewithcs <shikhar1chaudhary@gmail.com>
2021-01-28 21:53:13 +0000

    Add code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/ArrayOfDoubledPairs.java
@@ -0,0 +1,19 @@
+package LeetCodePremium.Medium;
+
+/*
+Given an array of integers arr of even length,
+return true if and only if
+it is possible to reorder it
+such that arr[2 * i + 1] = 2 * arr[2 * i]
+for every 0 <= i < len(arr) / 2.
+
+Constraints:
+0 <= arr.length <= 3 * 10^4
+arr.length is even.
+-10^5 <= arr[i] <= 10^5
+ */
+public class ArrayOfDoubledPairs {
+    public boolean canReorderDoubled(int[] arr) {
+        return false;
+    }
+}
