0e5d9138f7d8cb9eaa2ddc88693b18c659161b11
codewithcs <shikhar1chaudhary@gmail.com>
2021-01-31 22:36:55 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/PairOfSongsWithTotalDurationsDivisibleBy60.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/PairOfSongsWithTotalDurationsDivisibleBy60.java
@@ -14,8 +14,11 @@ Constraints:
 1 <= time[i] <= 500
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PairOfSongsWithTotalDurationsDivisibleBy60 {
-    // Output Limit Exceeded. 
+    // Output Limit Exceeded.
     public int numPairsDivisibleBy60(int[] time) {
         int pairs = 0;
         for(int i=0; i< time.length-1; i++){
@@ -26,5 +29,33 @@ public class PairOfSongsWithTotalDurationsDivisibleBy60 {
             }
         }
         return pairs;
+    }
+
+    // Based on 2-Sum logic. 
+    public int numPairsDivisibleBy60_2(int[] time) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int t : time) {
+            if(t % 60 == 0) {
+                count += map.getOrDefault(0, 0);
+            } else {
+                count += map.getOrDefault(60 - t % 60, 0);
+            }
+
+            map.put(t % 60, map.getOrDefault(t % 60, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public int numPairsDivisibleBy60_3(int[] time) {
+        int array[] = new int[60];
+        int retVal = 0;
+        for(int i : time) {
+            int n = i % 60;
+            retVal += array[n==0 ? 0 : 60-n];
+            array[n]++;
+        }
+        return retVal;
     }
 }
