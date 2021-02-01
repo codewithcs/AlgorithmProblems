c6c7a640983a4bc6e84ab0e8cf32add21b48b567
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-01 02:03:53 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,20 @@
+package LeetCodePremium.Medium;
+
+/*
+Given an integer array nums, you need to find one continuous
+subarray that if you only sort this subarray in ascending order,
+then the whole array will be sorted in ascending order.
+
+Return the shortest such subarray and output its length.
+
+Constraints:
+1 <= nums.length <= 10^4
+-10^5 <= nums[i] <= 10^5
+
+Follow up: Can you solve it in O(n) time complexity?
+ */
+public class ShortestUnsortedContinuousSubarray {
+    public int findUnsortedSubarray(int[] nums) {
+        return 0;
+    }
+}
