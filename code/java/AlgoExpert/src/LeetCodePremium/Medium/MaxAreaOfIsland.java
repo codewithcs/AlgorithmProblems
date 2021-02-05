8b3f0603638ba134d586dc10a778046c79e4fed0
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-05 08:02:52 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/MaxAreaOfIsland.java
@@ -0,0 +1,17 @@
+package LeetCodePremium.Medium;
+
+/*
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's
+(representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array.
+(If there is no island, the maximum area is 0.)
+
+Note: The length of each dimension in the given grid does not exceed 50.
+ */
+public class MaxAreaOfIsland {
+    public int maxAreaOfIsland(int[][] grid) {
+        return 0;
+    }
+}
