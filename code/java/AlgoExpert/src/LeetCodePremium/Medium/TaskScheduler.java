e51e25f2aac2507aa529bbf4560203da439b22a1
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-05 17:19:57 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/TaskScheduler.java
@@ -0,0 +1,25 @@
+package LeetCodePremium.Medium;
+
+/*
+Given a characters array tasks, representing the tasks a CPU needs to do,
+where each letter represents a different task. Tasks could be done in any order.
+Each task is done in one unit of time. For each unit of time, the CPU could
+complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown period
+between two same tasks (the same letter in the array), that is that there must be at
+least n units of time between any two same tasks.
+
+Return the least number of units of times that the
+CPU will take to finish all the given tasks.
+
+Constraints:
+1 <= task.length <= 104
+tasks[i] is upper-case English letter.
+The integer n is in the range [0, 100].
+ */
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        return 0;
+    }
+}
--- code/java/AlgoExpert/src/LeetCodePremium/Medium/TopKFrequentWords.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/TopKFrequentWords.java
@@ -17,10 +17,15 @@ Follow up:
 Try to solve it in O(n log k) time and O(n) extra space.
  */
 public class TopKFrequentWords {
+
+    // k<=N
     public List<String> topKFrequent(String[] words, int k) {
         List<String> result = new ArrayList<>();
+
+        // O(n) space.
         Map<String, Integer> map = new HashMap<>();
 
+        // O(n) time
         for(String word: words){
             if(!map.containsKey(word)){
                 map.put(word, 1);
@@ -29,6 +34,7 @@ public class TopKFrequentWords {
             }
         }
 
+        // Max Heap.
         PriorityQueue<String> priorityQueue = new PriorityQueue<>(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -45,6 +51,7 @@ public class TopKFrequentWords {
         // for(String key: map.keySet()){
         //     priorityQueue.add(key);
         // }
+
         priorityQueue.addAll(map.keySet());
 
         int size = 0;
@@ -53,6 +60,56 @@ public class TopKFrequentWords {
             size++;
         }
 
+        return result;
+    }
+
+    public List<String> topKFrequent2(String[] words, int k) {
+        List<String> result = new ArrayList<>();
+
+        // O(n) space.
+        Map<String, Integer> map = new HashMap<>();
+
+        // O(n) time
+        for(String word: words){
+            if(!map.containsKey(word)){
+                map.put(word, 1);
+            } else{
+                map.put(word, map.get(word) + 1);
+            }
+        }
+
+        // Min Heap
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int diff = map.get(o1) - map.get(o2);
+                if(diff == 0){
+                    return o2.compareTo(o1); // imp to think this. Keep bigger closer to the root when the count is equal.
+                } else {
+                    return diff;
+                }
+            }
+        });
+
+        // We only keep k  elements in the heap rather than adding all of them.
+        // O(N*log k) operation.
+        for(String key: map.keySet()){
+            priorityQueue.add(key);
+            if(priorityQueue.size() > k){
+                priorityQueue.poll(); // remove from the head.
+            }
+        }
+
+        /// We will still have k most frequent words in the heap
+        // Only difference will be that the head will contain the word with
+        // least frequency.
+
+        // Or can do result.add(0, priorityQueue.poll());
+        while(!priorityQueue.isEmpty()){ // Get the topmost k elements from the heap.
+            result.add(priorityQueue.poll());
+        }
+
+        Collections.reverse(result); // O(k*log k)
         return result;
     }
 }
