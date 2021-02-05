6ac6f69c7baf42ab46ec409493f174edf8c9e58b
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-05 09:47:25 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/LargestSubmatrixWithRearrangements.java
@@ -0,0 +1,21 @@
+package LeetCodePremium.Medium;
+
+
+/*
+You are given a binary matrix matrix of size m x n, and you are allowed to rearrange
+the columns of the matrix in any order.
+
+Return the area of the largest submatrix within matrix where every element of the
+submatrix is 1 after reordering the columns optimally.
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m * n <= 10^5
+matrix[i][j] is 0 or 1.
+ */
+public class LargestSubmatrixWithRearrangements {
+    public int largestSubmatrix(int[][] matrix) {
+        return 0;
+    }
+}
