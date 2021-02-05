d91e37789e719b708294134070839670364a436a
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-05 16:19:34 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/TopKFrequentWords.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/TopKFrequentWords.java
@@ -1,6 +1,6 @@
 package LeetCodePremium.Medium;
 
-import java.util.List;
+import java.util.*;
 
 /*
 Given a non-empty list of words, return the k most frequent elements.
@@ -18,6 +18,41 @@ Try to solve it in O(n log k) time and O(n) extra space.
  */
 public class TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
-        return null;
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String word: words){
+            if(!map.containsKey(word)){
+                map.put(word, 1);
+            } else{
+                map.put(word, map.get(word) + 1);
+            }
+        }
+
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int diff = map.get(o2) - map.get(o1);
+                if(diff == 0){
+                    return o1.compareTo(o2);
+                } else {
+                    return diff;
+                }
+            }
+        });
+
+        // Replacing the for loop with addAll
+        // for(String key: map.keySet()){
+        //     priorityQueue.add(key);
+        // }
+        priorityQueue.addAll(map.keySet());
+
+        int size = 0;
+        while(size < k){ // Get the topmost k elements from the heap.
+            result.add(priorityQueue.poll());
+            size++;
+        }
+
+        return result;
     }
 }
