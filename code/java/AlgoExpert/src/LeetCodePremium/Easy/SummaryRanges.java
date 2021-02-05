fde3080ebdcb431ae4cb9679b8cb675edbd2d1a7
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-05 07:52:25 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Easy/SummaryRanges.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Easy/SummaryRanges.java
@@ -55,6 +55,8 @@ public class SummaryRanges {
         return result;
     }
 
+
+    /// Using String Builder
     public List<String> summaryRanges2(int[] nums) {
         List<String> result = new ArrayList<>();
 
@@ -86,5 +88,24 @@ public class SummaryRanges {
         }
 
         return result;
+    }
+
+    // Nice Solution:
+    public List<String> summaryRanges3(int[] nums) {
+        List<String> summary = new ArrayList<>();
+        for (int i, j = 0; j < nums.length; ++j){
+            i = j;
+            // try to extend the range [nums[i], nums[j]]
+            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1) {
+                ++j;
+            }
+            // put the range into the list
+            if (i == j) {
+                summary.add(nums[i] + "");
+            } else {
+                summary.add(nums[i] + "->" + nums[j]);
+            }
+        }
+        return summary;
     }
 }
