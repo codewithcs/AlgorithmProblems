a954e59c5c959934fb7fd46e8e3d079aa1a5fdb0
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-05 12:59:07 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/TopKFrequentWords.java
@@ -0,0 +1,23 @@
+package LeetCodePremium.Medium;
+
+import java.util.List;
+
+/*
+Given a non-empty list of words, return the k most frequent elements.
+
+Your answer should be sorted by frequency from highest to lowest.
+If two words have the same frequency,
+then the word with the lower alphabetical order comes first.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Input words contain only lowercase letters.
+
+Follow up:
+Try to solve it in O(n log k) time and O(n) extra space.
+ */
+public class TopKFrequentWords {
+    public List<String> topKFrequent(String[] words, int k) {
+        return null;
+    }
+}
