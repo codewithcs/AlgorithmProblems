43cb649ed6d8578f0d28fb91bd07008159abe9ed
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-06 20:46:44 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/TaskScheduler.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/TaskScheduler.java
@@ -1,5 +1,9 @@
 package LeetCodePremium.Medium;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /*
 Given a characters array tasks, representing the tasks a CPU needs to do,
 where each letter represents a different task. Tasks could be done in any order.
@@ -14,12 +18,28 @@ Return the least number of units of times that the
 CPU will take to finish all the given tasks.
 
 Constraints:
-1 <= task.length <= 104
+1 <= tasks.length <= 10^4
 tasks[i] is upper-case English letter.
 The integer n is in the range [0, 100].
  */
 public class TaskScheduler {
     public int leastInterval(char[] tasks, int n) {
-        return 0;
+        int[] frequencies = new int[26];
+        for (char t : tasks) {
+            frequencies[t - 'A']++;
+        }
+
+        Arrays.sort(frequencies);
+
+        // max frequency
+        int f_max = frequencies[25];
+        int idle_time = (f_max - 1) * n;
+
+        for (int i = frequencies.length - 2; i >= 0 && idle_time > 0; --i) {
+            idle_time -= Math.min(f_max - 1, frequencies[i]);
+        }
+        idle_time = Math.max(0, idle_time);
+
+        return idle_time + tasks.length;
     }
 }
