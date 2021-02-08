385a1c6a6f2d83417a922d7cc20c3f9898c5f60c
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-08 09:55:58 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveAllAdjacentDuplicatesInString2.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveAllAdjacentDuplicatesInString2.java
@@ -1,5 +1,7 @@
 package LeetCodePremium.Medium;
 
+import javafx.util.Pair;
+
 /*
 Given a string s, a k duplicate removal consists of choosing k adjacent and
 equal letters from s and removing them causing the left and the right side
@@ -19,7 +21,36 @@ Constraints:
 s only contains lower case English letters.
 */
 public class RemoveAllAdjacentDuplicatesInString2 {
+
+    // Check All substrings of length k ?
     public String removeDuplicates(String s, int k) {
-        return "";
+        if(k > s.length()){
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+        boolean foundAtLeastOne = false;
+
+        do{
+            foundAtLeastOne = false;
+
+            for(int i=0; i< s.length()-k+1; i++) {
+                boolean found = true;
+                char current = s.charAt(i);
+                for(int j = i+1; j<=i+k-1 ; j++){
+                    if(current != sb.charAt(j)){
+                        found = false;
+                        break;
+                    }
+                }
+
+                if(found){
+                    sb = new StringBuilder(sb.substring(0, i) + sb.substring(i+k));
+                    foundAtLeastOne = true;
+                }
+            }
+        } while(foundAtLeastOne);
+
+        return sb.toString();
     }
 }
