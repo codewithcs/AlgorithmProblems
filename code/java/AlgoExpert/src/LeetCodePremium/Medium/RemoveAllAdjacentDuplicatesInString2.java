a11571f19a800c0d2c21e1ecab0e0b064dd7a071
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-09 22:38:03 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveAllAdjacentDuplicatesInString2.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveAllAdjacentDuplicatesInString2.java
@@ -1,5 +1,7 @@
 package LeetCodePremium.Medium;
 
+import java.util.Stack;
+
 /*
 Given a string s, a k duplicate removal consists of choosing k adjacent and
 equal letters from s and removing them causing the left and the right side
@@ -65,9 +67,10 @@ public class RemoveAllAdjacentDuplicatesInString2 {
                 count[i] = count[i-1] + 1;
                 if(count[i] == k){
                     sb.delete(i-k+1, i+1);
-                    i = i - k;
+                    i = i - k; // this line make the time complexity O(n).
+
                     /* count = new int[sb.length()]; // No need to re-initialize.
-                    if(sb.length() == 0){
+                    if(sb.length() == 0){ // using this logic, time complexity will be O(n^2/k)
                         break;
                     }
                     count[0] = 1;
@@ -99,5 +102,80 @@ public class RemoveAllAdjacentDuplicatesInString2 {
         return sb.toString();
     }
 
-
+    // Approach 3: Stack, O(n) time and O(n) space.
+    public String removeDuplicates3(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> counts = new Stack<>();
+        for (int i = 0; i < sb.length(); ++i) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                counts.push(1);
+            } else {
+                int incremented = counts.pop() + 1;
+                if (incremented == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                } else {
+                    counts.push(incremented);
+                }
+            }
+        }
+        return sb.toString();
     }
+
+
+    class Pair {
+        int cnt;
+        char ch;
+        public Pair(int cnt, char ch) {
+            this.ch = ch;
+            this.cnt = cnt;
+        }
+    }
+
+    // Stack With Re-Construction, O(n) time and O(n) space.
+    public String removeDuplicates4(String s, int k) {
+        Stack<Pair> counts = new Stack<>();
+        for (int i = 0; i < s.length(); ++i) {
+            if (counts.empty() || s.charAt(i) != counts.peek().ch) {
+                counts.push(new Pair(1, s.charAt(i)));
+            } else {
+                if (++counts.peek().cnt == k) {
+                    counts.pop();
+                }
+            }
+        }
+
+        StringBuilder b = new StringBuilder();
+
+        while (!counts.empty()) {
+            Pair p = counts.pop();
+            for (int i = 0; i < p.cnt; i++) {
+                b.append(p.ch);
+            }
+        }
+
+        return b.reverse().toString();
+    }
+
+
+    // 2 Pointer, Slow and Fast pointers, O(n) time and O(n) space. 
+    public String removeDuplicates5(String s, int k) {
+        Stack<Integer> counts = new Stack<>();
+        char[] sa = s.toCharArray();
+        int j = 0;
+        for (int i = 0; i < s.length(); ++i, ++j) {
+            sa[j] = sa[i];
+            if (j == 0 || sa[j] != sa[j - 1]) {
+                counts.push(1);
+            } else {
+                int incremented = counts.pop() + 1;
+                if (incremented == k) {
+                    j = j - k;
+                } else {
+                    counts.push(incremented);
+                }
+            }
+        }
+        return new String(sa, 0, j);
+    }
+}
