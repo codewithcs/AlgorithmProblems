d939b8d27a34c6bc82b923a5313ce67298fd1469
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-09 22:00:27 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveAllAdjacentDuplicatesInString2.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveAllAdjacentDuplicatesInString2.java
@@ -23,6 +23,9 @@ public class RemoveAllAdjacentDuplicatesInString2 {
         System.out.println( removeDuplicates("deeedbbcccbdaa", 3));
     }
     // Check All substrings of length k ?
+    // O(n^2) because of substrings ?
+
+    // Time Complexity ??????
     public static String removeDuplicates(String s, int k) {
         // Ask the interviewer about the case: k > s.length(), I think we should return s.
         StringBuilder sb = new StringBuilder(s);
@@ -50,4 +53,49 @@ public class RemoveAllAdjacentDuplicatesInString2 {
         return sb.toString();
     }
 
-}
+
+    // Approach 2:
+    public String removeDuplicates2(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        int[] count = new int[sb.length()];
+        count[0] = 1;
+        for(int i=1; i< sb.length(); i++){
+            if(sb.charAt(i) == sb.charAt(i-1)){
+                count[i] = count[i-1] + 1;
+                if(count[i] == k){
+                    sb.delete(i-k+1, i+1);
+                    count = new int[sb.length()];
+                    if(sb.length() == 0){
+                        break;
+                    }
+                    count[0] = 1;
+                    i=0;  // Important Step as we will need to re-fill the count values.
+                }
+            } else {
+                count[i] = 1;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // Using a single variable for the counts.
+    public String removeDuplicates2_Optimized(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        int length = -1;
+        while (length != sb.length()) {
+            length = sb.length();
+            for (int i = 0, count = 1; i < sb.length(); ++i) {
+                if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                    count = 1;
+                } else if (++count == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+
+    }
