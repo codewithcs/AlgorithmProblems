a4b37c441f1575b43b2e1879b65a85d67fd4f10a
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-10 12:31:03 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,15 @@
+package LeetCodePremium.Medium;
+
+/*
+Return the lexicographically smallest subsequence of s
+that contains all the distinct characters of s exactly once.
+
+Constraints:
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+ */
+public class SmallestSubsequenceOfDistinctCharacters {
+    public String smallestSubsequence(String s) {
+        return ""; 
+    }
+}
