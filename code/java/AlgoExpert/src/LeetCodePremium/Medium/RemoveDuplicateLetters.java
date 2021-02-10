eed2b5da108b6d4b9e899a828e1ff359988efa9f
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-10 13:25:31 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveDuplicateLetters.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/RemoveDuplicateLetters.java
@@ -1,5 +1,9 @@
 package LeetCodePremium.Medium;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /*
 Given a string s, remove duplicate letters so that every
 letter appears once and only once.
@@ -13,6 +17,30 @@ s consists of lowercase English letters.
  */
 public class RemoveDuplicateLetters {
     public String removeDuplicateLetters(String s) {
-        return "";
+        Map<Character, Integer> count = new HashMap<>();
+        StringBuilder sb = new StringBuilder(s);
+
+        for(char c: s.toCharArray()){
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c: s.toCharArray()){
+            if(count.get(c) == 1 ){
+
+            } else {
+
+            }
+        }
+
+
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+
+        return result.toString();
     }
 }
