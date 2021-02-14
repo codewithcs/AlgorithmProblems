22266d9deab85b80fde13ae02fe06b3eb72af474
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-14 16:09:47 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/LongestSubarrayOf1sAfterDeletingOneElement.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -19,15 +19,15 @@ public class LongestSubarrayOf1sAfterDeletingOneElement {
         // lengths[i]: Length of chain ending at index i.
         int maxLength = 0;
 
-        for(int i=0; i< nums.length; i++){
-            if(i==0){
-                if(nums[i] == 1){
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                if (nums[i] == 1) {
                     lengths[i] = 1;
                 }
             } else {
-                if(nums[i] == 1){
-                    if(nums[i-1] == 1){
-                        lengths[i] = 1 + lengths[i-1];
+                if (nums[i] == 1) {
+                    if (nums[i - 1] == 1) {
+                        lengths[i] = 1 + lengths[i - 1];
                     } else {
                         lengths[i] = 1;
                     }
@@ -37,16 +37,16 @@ public class LongestSubarrayOf1sAfterDeletingOneElement {
         }
 
         // Have to delete at least 1 element
-        if(maxLength == nums.length) {
+        if (maxLength == nums.length) {
             return maxLength - 1;
         }
 
-        for(int i=1; i< nums.length-1 ; i++){
-            if(nums[i] == 0){
-                if(nums[i+1] == 1 && nums[i-1] == 1){
-                    int currentLength = 1 + lengths[i-1]; // have to delete the 0, that is why we add just 1 and not 2.
-                    for(int j=i+2; j< nums.length; j++){
-                        if(nums[j] != 1){
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                if (nums[i + 1] == 1 && nums[i - 1] == 1) {
+                    int currentLength = 1 + lengths[i - 1]; // have to delete the 0, that is why we add just 1 and not 2.
+                    for (int j = i + 2; j < nums.length; j++) {
+                        if (nums[j] != 1) {
                             break;
                         } else {
                             currentLength++; // continuous chain
@@ -81,22 +81,23 @@ public class LongestSubarrayOf1sAfterDeletingOneElement {
     }
 
     // Sliding Window Solution: See Max Consecutive Ones III problem.
+    // Here we have to delete a 0 and not replace.
     public int longestSubarray3(int[] A) {
-        int i = 0;
-        int k = 1;
+        int left = 0;
+        int k = 1; // What if k > 1 ? 
         int res = 0;
 
-        for (int j = 0; j < A.length; ++j) {
-            if (A[j] == 0) {
+        for (int right = 0; right < A.length; ++right) {
+            if (A[right] == 0) {
                 k--;
             }
-            while (k < 0) {
-                if (A[i] == 0) {
+            while (k < 0) { // k<0 means that we have deleted all the ks.
+                if (A[left] == 0) {
                     k++;
                 }
-                i++;
+                left++;
             }
-            res = Math.max(res, j - i);
+            res = Math.max(res, right - left);
         }
 
         return res;
--- code/java/AlgoExpert/src/LeetCodePremium/Medium/MaxConsecutiveOnesII.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/MaxConsecutiveOnesII.java
@@ -14,7 +14,76 @@ In other words, you can't store all numbers coming from the stream as
 it's too large to hold in memory. Could you solve it efficiently?
  */
 public class MaxConsecutiveOnesII {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        return 0; 
+
+    // O(n^2) time and O(1) space.
+    public int findMaxConsecutiveOnes1(int[] nums) {
+        int longestSequence = 0;
+        for (int left = 0; left < nums.length; left++) {
+            int numZeroes = 0;
+
+            // check every consecutive sequence
+            for (int right = left; right < nums.length; right++) {
+                // count how many 0's
+                if (nums[right] == 0) {
+                    numZeroes += 1;
+                }
+                // # update answer if it's valid
+                if (numZeroes <= 1) {
+                    longestSequence = Math.max(longestSequence, right - left + 1);
+                }
+            }
+        }
+        return longestSequence;
+    }
+
+    // Sliding window 1.
+    public int findMaxConsecutiveOnes2(int[] nums) {
+        int longestSequence = 0;
+        int left = 0;
+        int right = 0;
+        int numZeroes = 0;
+
+        // while our window is in bounds
+        while (right < nums.length) {
+
+            // add the right most element into our window
+            if (nums[right] == 0) {
+                numZeroes++;
+            }
+
+            // if our window is invalid, contract our window
+            while (numZeroes == 2) {
+                if (nums[left] == 0) {
+                    numZeroes--;
+                }
+                left++;
+            }
+
+            // update our longest sequence answer
+            longestSequence = Math.max(longestSequence, right - left + 1);
+
+            // expand our window
+            right++;
+        }
+        return longestSequence;
+    }
+
+    // Sliding window 2.
+    public int findMaxConsecutiveOnes3(int[] nums) {
+        int left=0; int right=0; int k= 1;
+
+        while(right < nums.length){
+            if(nums[right] == 0){
+                k--;
+            }
+
+            if(k< 0){
+                if(nums[left] == 0) k++;
+                left++;
+            }
+            right++;
+        }
+
+        return right - left;
     }
 }
