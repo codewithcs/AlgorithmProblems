28360a3b1f2f14400d942acea5a0405b89aa8e4b
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-14 23:30:51 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/ConstructBinaryTreeFromPreOrderAndInOrderTraversal.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/ConstructBinaryTreeFromPreOrderAndInOrderTraversal.java
@@ -1,12 +1,15 @@
 package LeetCodePremium.Medium;
 
 /*
-Given preorder and inorder traversal of a tree,
+Given pre order and inorder traversal of a tree,
 construct the binary tree.
 
 Note:
 You may assume that duplicates do not exist in the tree.
  */
+
+import java.util.HashMap;
+import java.util.Map;
 
 class TreeNode{
     int val;
@@ -18,7 +21,98 @@ class TreeNode{
     }
 }
 public class ConstructBinaryTreeFromPreOrderAndInOrderTraversal {
+
+    // Approach 1: Simple Recursive Solution.
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = 0; // Index of current root in inorder
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                inIndex = i;
+            }
+        }
+
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+
+        return root;
+    }
+
+    // Approach 2:
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+
+        for(int i = 0; i < inorder.length; i++) {
+            inMap.put(inorder[i], i);
+        }
+
+        TreeNode root = buildTree2(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inMap);
+        return root;
+    }
+
+    public TreeNode buildTree2(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if(preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inRoot = inMap.get(root.val);
+        int numsLeft = inRoot - inStart;
+
+        root.left = buildTree2(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+        root.right = buildTree2(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+
+        return root;
+    }
+
+
+    // Approach 3:
+    // start from first preorder element
+    int pre_idx = 0;
+    int[] preorder;
+    int[] inorder;
+    HashMap<Integer, Integer> idx_map = new HashMap<>();
+
+    public TreeNode helper(int in_left, int in_right) {
+        // if there is no elements to construct subtrees
+        if (in_left == in_right) {
+            return null;
+        }
+
+        // pick up pre_idx element as a root
+        int root_val = preorder[pre_idx];
+        TreeNode root = new TreeNode(root_val);
+
+        // root splits inorder list
+        // into left and right subtrees
+        int index = idx_map.get(root_val);
+
+        // recursion
+        pre_idx++;
+        // build left subtree
+        root.left = helper(in_left, index);
+        // build right subtree
+        root.right = helper(index + 1, in_right);
+        return root;
+    }
+
+    public TreeNode buildTree3(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+
+        // build a hashmap value -> its index
+        int idx = 0;
+        for (Integer val : inorder) {
+            idx_map.put(val, idx++);
+        }
+        return helper(0, inorder.length);
     }
 }
