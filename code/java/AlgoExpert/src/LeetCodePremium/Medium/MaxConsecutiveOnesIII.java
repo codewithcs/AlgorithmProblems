f24e76b23d312b5bd84e0956e10cae3683d6f49b
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-14 15:16:58 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/MaxConsecutiveOnesIII.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/MaxConsecutiveOnesIII.java
@@ -27,8 +27,9 @@ public class MaxConsecutiveOnesIII {
             if (A[right] == 0) {
                 K--;
             }
-            
+
             // A negative K denotes we have consumed all allowed flips and window has
+
             // more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
             if (K < 0) {
                 // If the left element to be thrown out is zero we increase K.
