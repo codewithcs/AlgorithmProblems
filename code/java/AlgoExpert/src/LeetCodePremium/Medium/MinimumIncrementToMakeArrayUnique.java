ea184356580c850cc80a30a5d8b708ef25478336
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-15 12:10:05 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,19 @@
+package LeetCodePremium.Medium;
+
+/*
+Given an array of integers A, a move consists of choosing any A[i],
+and incrementing it by 1.
+
+Return the least number of moves to make every value in A unique.
+
+Twitter, Uber.
+
+Note:
+0 <= A.length <= 40000
+0 <= A[i] < 40000
+ */
+public class MinimumIncrementToMakeArrayUnique {
+    public int minIncrementForUnique(int[] A) {
+        return 0;
+    }
+}
