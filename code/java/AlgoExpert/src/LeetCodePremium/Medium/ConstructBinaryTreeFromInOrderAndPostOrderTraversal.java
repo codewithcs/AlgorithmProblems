bf2414368e7955f2ff4b47d17caf21985e0ff953
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-15 11:49:30 +0000

    Add Code

---
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/ConstructBinaryTreeFromInOrderAndPostOrderTraversal.java
@@ -0,0 +1,24 @@
+package LeetCodePremium.Medium;
+
+/*
+Given inorder and postorder traversal of a tree,
+construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+ */
+
+public class ConstructBinaryTreeFromInOrderAndPostOrderTraversal {
+    class TreeNode{
+        int val;
+        LeetCodePremium.Medium.TreeNode left;
+        LeetCodePremium.Medium.TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
