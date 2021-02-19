1501b999ff51603a155f4cbdec6b3b40f05c0491
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-19 16:30:34 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/MinimumIncrementToMakeArrayUnique.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/MinimumIncrementToMakeArrayUnique.java
@@ -16,14 +16,13 @@ Note:
  */
 
 public class MinimumIncrementToMakeArrayUnique {
-    // Time Limit Exceeded.
+    // Time Limit Exceeded. Do not need to sort as it does not help.
     public int minIncrementForUnique(int[] A) {
         if (A.length == 0) {
             return 0;
         }
 
         Set<Integer> unique = new HashSet<>();
-        Arrays.sort(A); /// O(n*log n)
         int minIncrement = 0;
         unique.add(A[0]);
         for (int i = 1; i < A.length; i++) {
@@ -37,39 +36,45 @@ public class MinimumIncrementToMakeArrayUnique {
         return minIncrement;
     }
 
-    // A: [3, 2, 1, 2, 1, 7], without sorting. 
+    // A: [3, 2, 1, 2, 1, 7].
+    // this solution is same as previous, using a map does not do anything better.
     public int minIncrementForUnique2(int[] A) {
-        if(A.length == 0){
+        if (A.length == 0) {
             return 0;
         }
 
         Set<Integer> unique = new HashSet<>();
         Map<Integer, Integer> count = new HashMap<>();
         int minIncrements = 0;
-        for(int num: A){
-            if(!count.containsKey(num)){
-                count.put(num , 1);
+        for (int num : A) {
+            if (!count.containsKey(num)) {
+                count.put(num, 1);
             } else {
-                count.put(num, count.get(num) + 1 );
+                count.put(num, count.get(num) + 1);
             }
         }
 
-        for(int num: A){
-            if(count.get(num) == 1){
+        for (int num : A) {
+            if (count.get(num) == 1) {
                 unique.add(num);
             }
         }
 
-        // unique: {3, 7}, duplicates: {1, 2}
-        for(int i=0; i< A.length; i++){
-            if(count.get(A[i]) > 1) { // checking on previous value.
-                while(unique.contains(A[i])){
-                    A[i]++; minIncrements++;
+        // unique: {3, 7}.
+        for (int i = 0; i < A.length; i++) {
+            if (count.get(A[i]) > 1) { // checking on previous value.
+                while (unique.contains(A[i])) {
+                    A[i]++;
+                    minIncrements++;
                 }
             }
             unique.add(A[i]); // In the end A[i] is a unique element of the set.
         }
 
         return minIncrements;
+    }
+
+    public int minIncrementForUnique3(int[] A) {
+        return 0; 
     }
 }
