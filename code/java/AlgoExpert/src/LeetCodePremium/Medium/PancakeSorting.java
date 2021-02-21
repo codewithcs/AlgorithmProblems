d8951bf431af5db9f5b17eaf1ff274ff06f0d448
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-21 17:53:27 +0000

    Add Code

--- code/java/AlgoExpert/src/LeetCodePremium/Medium/PancakeSorting.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Medium/PancakeSorting.java
@@ -24,6 +24,8 @@ All integers in arr are unique (i.e. arr is a permutation of the integers from 1
  */
 
 public class PancakeSorting {
+
+    // gets accepted, 5%.
     public List<Integer> pancakeSort(int[] arr) {
         List<Integer> result = new ArrayList<>();
 
@@ -33,18 +35,24 @@ public class PancakeSorting {
         while(index != -1){
             reverse(arr, index);
             result.add(index+1);
+            result.add(arr[0]); // Do this before the 2nd reversal as we are adding an array element which can
+                                // change after the 2nd reversal.
             reverse(arr, arr[0]-1);
-            result.add(arr[0]);
             index = findIncorrectIndex(arr);
         }
 
+        // If index is -1 then it means that every element is at their
+        // correct position and hence the array is sorted.
         return result;
     }
 
+    // Find the incorrect index of the maximum element in the array.
     public int findIncorrectIndex(int[] arr){
         int max = 0;
         int maxIndex = -1;
 
+        // We calculate both max number and its index.
+        // See approach 2.
         for(int i=0 ; i<= arr.length-1 ; i++){
             if(arr[i] != i+1){
                 if(arr[i] > max){
@@ -68,51 +76,10 @@ public class PancakeSorting {
         }
     }
 
-    /// Approach 2:
+    // Approach 2: array value in [1, arr.length], 100%
+    // This approach makes use of the given constraint.
+    // We already know the max number, we just need to find its index.
     public List<Integer> pancakeSort2(int[] arr) {
-        List<Integer> result = new ArrayList<>();
-
-        // Find the largest number not at correct index.
-        for(int i=0 ; i< arr.length ; i++){
-            int maxNumberIndex = findMaximumIndex(i, arr);
-            reverse(arr, i, maxNumberIndex);
-            result.add(maxNumberIndex+1);
-        }
-
-        reverse(arr, 0, arr.length-1);
-
-        result.add(arr.length);
-        return result;
-    }
-
-    public int findMaximumIndex(int start, int[] arr){
-        int max = arr[start];
-        int maxIndex = -1;
-
-        for(int i=start+1 ; i<= arr.length-1 ; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                maxIndex = i;
-            }
-        }
-
-        return maxIndex;
-    }
-
-    public void reverse(int[] arr, int start, int index){
-        int left = start ;
-        int right = index;
-
-        while(left < right){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++; right--;
-        }
-    }
-
-    // Approach 3: array value in [1, arr.length]
-    public List<Integer> pancakeSort3(int[] arr) {
         List<Integer> result = new ArrayList<>();
 
         for(int i= arr.length; i>=1; i--){
@@ -150,4 +117,55 @@ public class PancakeSorting {
         return index;
     }
 
+    /*
+    //----------------------------------------------------------------------------------------------------------
+    /// This is not Pancake Sort. [1, 5, 6, 7, 8, 2, 3, 4], but sorts the array.
+    public static List<Integer> notPancakeSort2(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+
+        // First I reverse sort the numbers and then do a final flip of the whole array.
+        for(int i=0 ; i< arr.length ; i++){
+            int maxNumberIndex = findMaximumIndex(i, arr);
+            if(maxNumberIndex == -1 ) {
+                continue;
+            }
+            reverse(arr, i, maxNumberIndex);
+            result.add(maxNumberIndex+1);
+        }
+
+        reverse(arr, 0, arr.length-1);
+        result.add(arr.length);
+        return result;
+    }
+
+    public static int findMaximumIndex(int start, int[] arr){
+        int max = arr[start];
+        int maxIndex = -1;
+
+        for(int i=start+1 ; i<= arr.length-1 ; i++){
+            if( i + arr[i] != arr.length) {
+                if(arr[i] > max){
+                    max = arr[i];
+                    maxIndex = i;
+                }
+            }
+        }
+
+        return maxIndex;
+    }
+
+    public static void reverse(int[] arr, int start, int index){
+        int left = start ;
+        int right = index;
+
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++; right--;
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------
+
+     */
 }
