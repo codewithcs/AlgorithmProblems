0d756fd4f9aaac6f3c12158f7db5e9ee50f75d59
codewithcs <shikhar1chaudhary@gmail.com>
2021-02-24 17:27:56 +0000

    Add Cod

--- code/java/AlgoExpert/src/LeetCodePremium/Easy/ThirdMaximumNumber.java
+++ code/java/AlgoExpert/src/LeetCodePremium/Easy/ThirdMaximumNumber.java
@@ -3,6 +3,10 @@ package LeetCodePremium.Easy;
 /*
 Given integer array nums, return the third maximum number in this array.
 If the third maximum does not exist, return the maximum number.
+
+Constraints:
+1 <= nums.length <= 10^4
+-2^31 <= nums[i] <= 2^31 - 1
  */
 public class ThirdMaximumNumber {
     public int thirdMax(int[] nums) {
